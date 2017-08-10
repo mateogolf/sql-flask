d8d828eee271414ce1879e8185cf47dc7995d10e
mateogolf <rodriguezml11@mail.wlu.edu>
2017-08-10 19:15:02 -0400

    Users Assignment adjustments

--- friends.sql
+++ friends.sql
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS `friendsdb`.`friends` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(45) NULL,
   `last_name` VARCHAR(45) NULL,
-  `occupation` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
   `created_at` DATETIME NULL,
   `updated_at` DATETIME NULL,
   PRIMARY KEY (`id`))
