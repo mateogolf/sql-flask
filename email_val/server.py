8c43af89646a4f177571f616ffbe9b85d4fdf729
mateogolf <rodriguezml11@mail.wlu.edu>
2017-08-10 12:11:23 -0400

    Updated validation function

--- email_val/server.py
+++ email_val/server.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 mysql = MySQLConnector(app, 'mydb')
 
 def validateEmail(email):
-	if len(email) > 7:
-		if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", email) != None:
-			return 1
-	return 0
+    if len(email) > 7:
+        if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", email) != None:
+            return True
+    return False
 
 @app.route('/')
 def index():
@@ -21,10 +21,18 @@ def home():
 def test():
     addressToVerify = request.form['email']
     match = validateEmail(addressToVerify)
+    #If comparing to database
+    # query = "SELECT id from emails WHERE name= :name"
+    # data = {'name': addressToVerify}
+    # input_IDs = mysql.query_db(query, data)
+    # print input_IDs
+    # if mysql.query_db(query, data):
+        # print "Email already exists"
 
-    if match == 0:
-        return render_template('index.html',Valid=False)
-    elif match == 1:
+
+    if not match: #Input is invalid
+        return render_template('index.html', tested=addressToVerify, Valid=False)
+    else:
         # this code for valid emails
         #INSERT INTO query
         query = "INSERT INTO emails (name,created_at, updated_at) VALUES (:name,NOW(),NOW())"
